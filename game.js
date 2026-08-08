6187454a516eece151c4ced63c68d5360febc24a
insightlogicil-cpu <insight.logic.il@gmail.com>
2026-08-08 13:31:56 +0300

    Replace truncated cow cinema narration

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const animals = [
 ];
 
 const $ = id => document.getElementById(id);
-const ASSET_VERSION = "20260808-4";
+const ASSET_VERSION = "20260808-5";
 let stage = 0, soundOn = true, musicOn = true, currentAnswer = 0, locked = false;
 let additionDeck = [], countingDeck = [];
 let currentQuestionSpeech = "";
