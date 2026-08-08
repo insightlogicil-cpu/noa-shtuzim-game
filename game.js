9ed83662636ca5d8786c1fc9fa72dc52da98d32d
insightlogicil-cpu <insight.logic.il@gmail.com>
2026-08-08 14:32:18 +0300

    Restore correct narration for sheep shtuz 14

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const animals = [
 ];
 
 const $ = id => document.getElementById(id);
-const ASSET_VERSION = "20260808-12";
+const ASSET_VERSION = "20260808-13";
 let stage = 0, soundOn = true, musicOn = true, currentAnswer = 0, locked = false;
 let additionDeck = [], countingDeck = [];
 let currentQuestionSpeech = "";
